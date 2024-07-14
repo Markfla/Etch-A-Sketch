a28d1b4099f6152fa0812f8325b9ef26d4a3878d
Markfla <147580286+Markfla@users.noreply.github.com>
2024-07-14 20:00:06 +0100

    got lazy and copied working finished copy

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-let container = document.getElementById("container");
-let select = document.getElementById("select");
-let reset = document.getElementById("reset");
+const sketchPad = document.querySelector('.sketchPad');
+const gridSize = document.querySelector('.menu button:nth-child(2)');
+const clearGrid = document.querySelector('.menu button:nth-child(3)');
+const deleteGrid = document.querySelector('.menu button:nth-child(4)');
 
-createGrid(16);
 
-select.addEventListener('click', () => {
-    resetGrid();
-    let num = parseInt(prompt("enter a number between 1 and 100 included"));
-    while(isNaN(num) || num > 100 || num < 0){
-        alert("Input error");
-        num = parseInt(prompt("Enter a number between 1 and 100 included"));
+function generateGrid(num) {
+    while (sketchPad.firstChild) {
+        sketchPad.removeChild(sketchPad.firstChild);
     }
-       createGrid(num);});
+    for (let i = 1; i <= num * num; i++) {
+        const childDiv = document.createElement('div')
+        childDiv.style.cssText = `width: ${479.5 / num}px; height: ${479.5 / num}px; outline: 1px solid #36454F;`
 
-// create the grid of specified size
-function createGrid(size){
-    for(let i=0; i < size; i++){
-        let row = document.createElement('div');
-        row.classList.add('grid-row');
-        container.appendChild(row);
-        for(let j=0; j < size; j++){
-            let cell = document.createElement('div');
-            cell.classList.add('grid-cell');
-            cell.addEventListener('mouseover', (e) => e.target.style.backgroundColor = randomColor());
-            row.appendChild(cell);
+        childDiv.addEventListener('mouseenter', () => {
+            childDiv.classList.add('childDivColor');
+        })
+
+        sketchPad.appendChild(childDiv)
+
+        clearGrid.addEventListener('click', () => {
+            childDiv.classList.remove('childDivColor')
+        })
     }
 }
-}
 
-// create and return a random color
-function randomColor(){
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
+gridSize.addEventListener('click', () => {
+    let num = +prompt(`Enter grid dimension`);
+    generateGrid(num);
+})
 
-    let color = "rgb(" + r + "," + g + "," + b + ")";
-    return color;
-}
-
-function resetGrid(){
-    container.innerHTML = '';
-}
-
-reset.addEventListener('click', () => resetGrid());
+deleteGrid.addEventListener('click', () => {
+    while (sketchPad.firstChild) {
+        sketchPad.removeChild(sketchPad.firstChild);
+    }
+})
